4706f8424d3345f27585c97c6bd9c6622b0da454
deisesalless <deisesalles18@gmail.com>
2025-08-13 21:53:46 -0300

    S do SOLID, nessa versão do pacote SRP estou obrigando que o método calcula bonificação seja implementado quando adicionar um novo cargo, evita bugs futuros por manutenção por outros desenvolvedores que não criaram esse código, só estão dando manutenção

--- src/br/com/deisesales/srp/Cargo.java
+++ src/br/com/deisesales/srp/Cargo.java
@@ -1,7 +1,24 @@
 package br.com.deisesales.srp;
 
 public enum Cargo {
-    ANALISTA,
-    DESENVOLVEDOR,
-    GERENTE;
+    ANALISTA {
+        @Override
+        public double calculaBonificacao(double salarioBase) {
+            return salarioBase * 0.05; // 5% para analistas
+        }
+    },
+    DESENVOLVEDOR {
+        @Override
+        public double calculaBonificacao(double salarioBase) {
+            return salarioBase * 0.01; // 1% para desenvolvedores
+        }
+    },
+    GERENTE {
+        @Override
+        public double calculaBonificacao(double salarioBase) {
+            return salarioBase * 0.1; // 10% para gerentes
+        }
+    };
+
+    public abstract double calculaBonificacao(double salarioBase);
 }
